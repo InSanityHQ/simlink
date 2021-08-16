92c1f0126387ba1991edea86eafb025c016ae2f9
Houjun Liu <hliu@shabang.cf>
2021-08-16 16:10:09 -0700

    welp I guess its time to make the perfect carrot

---
+++ src/simlink/views/default.cljs
@@ -0,0 +1,6 @@
+(ns simlink.views.default)
+
+(defn render []
+  [:div 
+   [:h1 "simlink Default View"]
+   [:p "It's good to see you"]])
---
+++ src/simlink/views/one.cljs
@@ -0,0 +1,6 @@
+(ns simlink.views.one)
+
+(defn render []
+  [:div 
+   [:h1 "View One"]
+   [:p "It's nice to see you"]])
